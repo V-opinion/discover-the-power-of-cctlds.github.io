0cde5769605cfc4f5b585544c75757dc8ee1c305
Eu-medborgarinitiativ <80989781+V-opinion@users.noreply.github.com>
2025-05-20 23:45:32 +0200

    Add files via upload

---
+++ Fattiga-ländernas-ccTLDs-discover-the-power-of-ccTLDs/uag-js-1932.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-448d1f84' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d2108615' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a526d836' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-1993a49a' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-4455b6a1' );
+});
+ });
